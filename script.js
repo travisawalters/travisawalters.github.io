3332e49a98d1c65ca872ab50c8a94e15ac3c0765
travisawalters <121747177+travisawalters@users.noreply.github.com>
2023-11-29 21:35:01 -0800

    actually putting in the CSS and JS

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let snapPoints = document.querySelectorAll('.page');
+    let currentSnapPoint = 0;
+    let isScrolling;
+    let lastWidth = window.innerWidth;
+    let zooming = false;
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth !== lastWidth) {
+            zooming = true;
+            document.body.style.scrollSnapType = 'none'; // Temporarily disable scroll snapping
+            lastWidth = window.innerWidth;
+        } else {
+            zooming = false;
+            document.body.style.scrollSnapType = 'y mandatory'; // Re-enable scroll snapping
+        }
+    });
+
+    window.addEventListener('wheel', function (e) {
+        if (zooming) return; // Don't snap if the user is zooming
+
+        clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(function () {
+            if (e.deltaY > 20 && currentSnapPoint < snapPoints.length - 1) {
+                currentSnapPoint++;
+            } else if (e.deltaY < -20 && currentSnapPoint > 0) {
+                currentSnapPoint--;
+            }
+            snapPoints[currentSnapPoint].scrollIntoView({behavior: 'smooth'});
+        }, 66);
+    });
+});
