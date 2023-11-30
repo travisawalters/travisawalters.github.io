c27007fdd4293ee96f3a14e86cb7cc79b90bc4e6
travisawalters <121747177+travisawalters@users.noreply.github.com>
2023-11-29 21:56:29 -0800

    Update script.js
    testing zoom

--- script.js
+++ script.js
@@ -2,32 +2,41 @@ document.addEventListener('DOMContentLoaded', function () {
     let snapPoints = document.querySelectorAll('.page');
     let currentSnapPoint = 0;
     let isScrolling;
-    let lastWidth = window.innerWidth;
     let zooming = false;
 
     window.addEventListener('resize', function() {
-        if (window.innerWidth !== lastWidth) {
-            zooming = true;
-            document.body.style.scrollSnapType = 'none';
-            lastWidth = window.innerWidth;
-        } else {
-            zooming = false;
-            document.body.style.scrollSnapType = 'y mandatory';
-        }
+        // Detect zoom by checking the change in window dimensions
+        zooming = checkZoom();
     });
 
     window.addEventListener('wheel', function (e) {
-        if (zooming) return;
+        if (zooming) return; // Don't snap if the user is zooming
 
         clearTimeout(isScrolling);
 
         isScrolling = setTimeout(function () {
-            if (e.deltaY > 20 && currentSnapPoint < snapPoints.length - 1) {
+            if (e.deltaY > 20 && currentSnapPoint < snapPoints.length - 1 && atPageEnd()) {
                 currentSnapPoint++;
-            } else if (e.deltaY < -20 && currentSnapPoint > 0) {
+            } else if (e.deltaY < -20 && currentSnapPoint > 0 && atPageStart()) {
                 currentSnapPoint--;
             }
             snapPoints[currentSnapPoint].scrollIntoView({behavior: 'smooth'});
         }, 66);
     });
+
+    function checkZoom() {
+        return window.outerWidth !== window.innerWidth;
+    }
+
+    function atPageEnd() {
+        // Custom logic to check if the user is at the end of the page
+        // This is a placeholder and needs to be refined based on actual requirements
+        return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+    }
+
+    function atPageStart() {
+        // Custom logic to check if the user is at the start of the page
+        // This is a placeholder and needs to be refined based on actual requirements
+        return window.scrollY === 0;
+    }
 });
